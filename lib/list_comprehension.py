56b97d677f99ecc1acd76f53fcb1ac98ca614828
Denzel <jdenzelmab@gmail.com>
2023-07-10 19:32:52 -0400

    Completed comprehensions and generators lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(num_list):   
+    n = [num for num in num_list if num % 2 == 0]
+    if 0 in n:
+        return n
+    else: return []
 
 def make_exclamation(sentence_list):
+    if sentence_list == []:
+        return []
+    else: 
+        n = [s + '!' for s in sentence_list ]
+        return n
     pass
